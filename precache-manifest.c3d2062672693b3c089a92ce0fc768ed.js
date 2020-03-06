3929bd63cc40957672fd8e19c09bd18b5b2abbbd
unknown <vuthao.ak56ht@gmail.com>
2020-03-06 16:40:56 +0700

    Updates

---
+++ precache-manifest.c3d2062672693b3c089a92ce0fc768ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c13f969086af7cc9b2fbf95f293bce",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2df0e663ffd68f29d5cf",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "644992cf89e1fb95a8d6",
+    "url": "/my-app/static/js/2.f8384d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.f8384d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df0e663ffd68f29d5cf",
+    "url": "/my-app/static/js/main.a6e9f100.chunk.js"
+  },
+  {
+    "revision": "1b58fcb5ce8d26145ee7",
+    "url": "/my-app/static/js/runtime-main.8b909c6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
